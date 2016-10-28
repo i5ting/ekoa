2bfff69a2f189348600ff8fceab8e70a95780df3
i5ting <i5ting@126.com>
2016-10-28 09:56:18 +0800

    lazycommmit, please ignore

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const Koa = require('koa')
 const app = new Koa()
 
+var hm = require('.')
+
+app.use(hm(function(req, res, next){
+  console.log('start')
+  res.body = "sss";
+
+  // return next().then(function (){
+  //   console.log('end')
+
+    
+  // })
+  
+}))
+
 // response
 app.use(ctx => {
   ctx.body = 'Hello Koa'
 })
 
 module.exports = app
+
+app.listen(4000)
--- index.js
+++ index.js
@@ -5,9 +5,26 @@
  * Module dependencies.
  */
 
-module.exports = (ctx, next) => {
-  var req = ctx.request
-  var res = ctx.response
+module.exports = (fn) => {
+  if (fn.length == 3) {
+    return (ctx, next) => {
+      var req = ctx.req
+      var res = ctx.response
   
-  hm(req, res, next)
+      fn.call(this, req, res, next)
+    } 
+  }
+  
+  return (ctx, next) => {
+    fn.apply(this, arguments) 
+  }
 }
+//
+// function gWrapper(fn) {
+//     if (fn.length == 4) return function(err, req, res, next) {
+//         return co(fn.call(this, err, req, res, next)).catch(next);
+//     }
+//     return function() {
+//         return co(fn.apply(this, arguments)).catch(arguments[arguments.length - 1]);
+//     }
+// }
